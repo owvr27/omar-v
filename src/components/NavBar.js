8acba27b80e841ed88ca232ac0a41ac4a7425c39
Omar Abdelsalam <oe7836196@gmail.com>
2025-10-03 15:05:04 +0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -33,7 +33,7 @@ class NavBar extends React.Component {
               <Nav.Link href="https://www.linkedin.com/in/omar-abdelsalam-29276a341/" target="_blank">
                 <LinkedInIcon style={{ fontSize: 21 }}></LinkedInIcon>
               </Nav.Link>
-              <Nav.Link href={process.env.PUBLIC_URL + "/assets/Omar_Abdelsalam.pdf"} download="Omar_Abdelsalam_CV.pdf">
+              <Nav.Link href={process.env.PUBLIC_URL + "/assets/OmarCv.pdf"} download="OmarCv.pdf">
                 <DescriptionIcon style={{ fontSize: 21 }}></DescriptionIcon>
               </Nav.Link>
               
